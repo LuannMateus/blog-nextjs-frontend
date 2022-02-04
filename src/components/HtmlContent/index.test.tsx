1a1aa16252a986d786ae5d6017d2e18c7d76d2a6
LuannMateus <luanmateuslima34@gmail.com>
2022-02-04 20:42:53 -0300

    Modify TextComponent by changing the name to HtmlContent

--- src/components/HtmlContent/index.test.tsx
+++ src/components/HtmlContent/index.test.tsx
@@ -1,16 +1,16 @@
 import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/renderTheme';
-import { TextComponent } from '.';
+import { HtmlContent } from '.';
 
-describe('<TextComponent/>', () => {
+describe('<TextCom  ponent/>', () => {
   it('Should render a paragraph with Children', () => {
-    renderTheme(<TextComponent>Children</TextComponent>);
+    renderTheme(<HtmlContent html="<b>Children</b>" />);
 
     expect(screen.getByText('Children')).toBeInTheDocument();
   });
 
   it('Should match snapshot', () => {
-    const { container } = renderTheme(<TextComponent>texto</TextComponent>);
+    const { container } = renderTheme(<HtmlContent html="<b>Children</b>" />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
