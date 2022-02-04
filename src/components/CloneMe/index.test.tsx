6b48dacb8f0677b813220980980fced40da86243
LuannMateus <luanmateuslima34@gmail.com>
2022-02-04 17:23:12 -0300

    Add Heading with unit tests

--- src/components/CloneMe/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { CloneMe } from '.';
-
-describe('<CloneMe />', () => {
-  it('Should render', () => {
-    render(<CloneMe />);
-  });
-});
