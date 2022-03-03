9f0fdc0d73e51ebf6db762e5b9770aefdb9c5163
LuannMateus <luanmateuslima34@gmail.com>
2022-03-03 15:38:04 -0300

    Fix Menu mock

--- src/components/Menu/mock.ts
+++ src/components/Menu/mock.ts
@@ -5,25 +5,25 @@ export default {
   links: [
     {
       id: 'link-1',
-      children: 'LINK 1',
+      text: 'LINK 1',
       newTab: false,
       link: 'https://www.google.com/',
     },
     {
       id: 'link-2',
-      children: 'LINK 2',
+      text: 'LINK 2',
       newTab: false,
       link: 'https://www.google.com/',
     },
     {
       id: 'link-3',
-      children: 'LINK 3',
+      text: 'LINK 3',
       newTab: false,
       link: 'https://www.google.com/',
     },
     {
       id: 'link-4',
-      children: 'LINK 4',
+      text: 'LINK 4',
       newTab: false,
       link: 'https://www.google.com/',
     },
